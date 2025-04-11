0d273468de3c1af285611aa9aa1d0dcd0a48d1ef
karolbaran <karolbaran@gmail.com>
2025-04-11 14:40:05 +0200

    subscribe endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,18 @@ async fn health() -> impl IntoResponse {
     StatusCode::OK
 }
 async fn subscribe(Form(data): Form<FormData>) -> impl IntoResponse {
-    dbg!(data);
-    StatusCode::CREATED
+    dbg!(&data);
+    if data.name.is_empty() && data.email.is_empty() {
+        return (StatusCode::BAD_REQUEST, "Name and email is required");
+    } else if data.name.is_empty() {
+        return (StatusCode::BAD_REQUEST, "Name is required");
+    } else if data.email.is_empty() {
+        return (StatusCode::BAD_REQUEST, "Email is required");
+    };
+    (StatusCode::CREATED, "success")
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct FormData {
     pub name: String,
     pub email: String,
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,5 @@
-use axum::serve::Listener;
+use std::collections::HashMap;
+
 use axum_leptos_zero2prod::FormData;
 use reqwest::{Client, StatusCode};
 use tokio::net::TcpListener;
@@ -28,11 +29,48 @@ async fn subscribe_return_200_for_a_valid_data() {
         .await
         .expect("Failed to send request");
 
-    assert_eq!(StatusCode::OK, response.status())
+    assert_eq!(StatusCode::CREATED, response.status())
 }
 
 #[tokio::test]
-async fn subscribe_return_400_when_data_is_missing() {}
+async fn subscribe_return_400_when_data_is_missing() {
+    let app = spawn_app().await;
+    let client = Client::new();
+
+    let form = FormData {
+        name: "karol".to_string(),
+        email: "karol@test.com".to_string(),
+    };
+
+    let mut invalid_forms = HashMap::new();
+    let mut one = form.clone();
+    let mut two = form.clone();
+    let mut three = form.clone();
+    one.email = "".to_owned();
+    two.name = "".to_owned();
+    three.email = "".to_owned();
+    three.name = "".to_owned();
+    invalid_forms.insert("missing the email", one);
+    invalid_forms.insert("missing the name", two);
+    invalid_forms.insert("missing both name and email", three);
+
+    for (error_message, invalid_form) in invalid_forms {
+        let response = client
+            .post(format!("{}/subscriptions", app))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .form(&invalid_form)
+            .send()
+            .await
+            .expect("Failed to send request");
+
+        assert_eq!(
+            StatusCode::BAD_REQUEST,
+            response.status(),
+            "The API did not faile with 400 Bad Request when the payload was {}",
+            error_message
+        )
+    }
+}
 
 #[tokio::test]
 async fn health_check_works() {
